31804e79b7249e88d6a6b6408a78ec6729d4aeb8
Диана <diana_yudina@mail.ru>
2022-03-31 21:20:41 +0500

    Homework_4.5 step 4 is done.

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.stream.Stream;
+
 @RestController
 @RequestMapping("/info")
 public class InfoController {
@@ -15,5 +17,13 @@ public class InfoController {
     @GetMapping("/port")
     public int getPort() {
         return port;
+    }
+
+    @GetMapping("/get_value")
+    public int getValue() {
+        return Stream.iterate(1, a -> a + 1)
+                .limit(1_000_000)
+                .parallel()
+                .reduce(0, Integer::sum);
     }
 }
